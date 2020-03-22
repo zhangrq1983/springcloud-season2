cefbf24e4ef1fb69ddf16591bab0c71259fe5560
zhangrq <zhangrongianglo@163.com>
2020-03-22 23:45:24 +0800

    更新

---
+++ cloud-consumer-feign-80/src/main/java/org/example/Consumer_Feign_80.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class Consumer_Feign_80 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Consumer_Feign_80.class, args);
+    }
+
+}
---
+++ cloud-consumer-feign-80/src/test/java/org/example/cloudconsumerfeign80/CloudConsumerFeign80ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.cloudconsumerfeign80;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudConsumerFeign80ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
