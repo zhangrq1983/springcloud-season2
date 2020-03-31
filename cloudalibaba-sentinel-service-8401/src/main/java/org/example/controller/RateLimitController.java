d86052b925df89df520e47b600cb86d696490e9c
zhangrq <zhangrongianglo@163.com>
2020-03-31 17:35:23 +0800

    更新

--- cloudalibaba-sentinel-service-8401/src/main/java/org/example/controller/RateLimitController.java
+++ cloudalibaba-sentinel-service-8401/src/main/java/org/example/controller/RateLimitController.java
@@ -21,6 +21,7 @@ public class RateLimitController
     {
         return new CommonResult(200,"按资源名称限流测试OK",new Payment(2020L,"serial001"));
     }
+
     public CommonResult handleException(BlockException exception)
     {
         return new CommonResult(444,exception.getClass().getCanonicalName()+"\t 服务不可用");
@@ -32,7 +33,6 @@ public class RateLimitController
     {
         return new CommonResult(200,"按url限流测试OK",new Payment(2020L,"serial002"));
     }
-
 
     @GetMapping("/rateLimit/customerBlockHandler")
     @SentinelResource(value = "customerBlockHandler",
