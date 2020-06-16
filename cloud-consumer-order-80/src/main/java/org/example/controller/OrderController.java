01376da51af766bb757cde73d0d76263d20a6c0a
zhangrq <zhangrongianglo@163.com>
2020-06-16 16:10:02 +0800

    更新

--- cloud-consumer-order-80/src/main/java/org/example/controller/OrderController.java
+++ cloud-consumer-order-80/src/main/java/org/example/controller/OrderController.java
@@ -8,10 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 import java.net.URI;
 import java.util.List;
@@ -44,7 +41,7 @@ public class OrderController {
         return restTemplate.getForObject(PAYMENT_URL + "/payment/get/" + id, CommonResult.class, id);
     }
 
-    @GetMapping("/payment/create")
+    @PostMapping("/payment/create")
     public CommonResult<Payment> create(Payment payment) {
         return restTemplate.postForObject(PAYMENT_URL + "/payment/create", payment, CommonResult.class);
     }
